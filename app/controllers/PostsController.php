4905ca94dbcfbd0508487030fe3c5fd752091956
Douglas Reith <douglas@reith.com.au>
2012-05-11 22:11:47 +1000

    Posts

---
+++ app/controllers/PostsController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\controllers;
+
+class PostsController extends \lithium\action\Controller {
+    public function index() {
+        return array('foo' => 'bar', 'title' => 'Posts');
+    }
+}
+
+?>
---
+++ app/views/posts/index.html.php
@@ -0,0 +1,1 @@
+    Lithium is less dense than <?=$foo;?>ium.
